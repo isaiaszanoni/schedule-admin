966f27d421ba86bd3e4fa3039fbdd646b21d163d
isaiaszanoni <isaiasz.mus@gmail.com>
2026-02-26 14:21:51 -0300

    SA-2: create event type

---
+++ src/types/event.ts
@@ -0,0 +1,10 @@
+type event = {
+    active: boolean;
+    local: string;
+    address: string;
+    buttonSrc?: string;
+    buttonText?: string;
+    date: string;
+    id: number;
+    title: string;
+}
